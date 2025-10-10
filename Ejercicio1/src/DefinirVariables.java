4824fbf7f783648cb76c84a7463330712f4fef5a
Luikky <Luikky@gmail.com>
2025-10-10 16:09:12 +0100

    update

---
+++ Ejercicio1/src/DefinirVariables.java
@@ -0,0 +1,19 @@
+public class DefinirVariables {
+    public static void main(String[] args)
+    {
+        // ------------ Ejercicio 1 ------------
+
+        int edad = 39;
+        String nombre = "Luis";
+        String ciudad = "S/C de Tenerife"
+        boolean mayorEdad = true;
+
+        System.out.println("Mi nombre es " + nombre + " tengo " + edad + " y soy de " + ciudad);
+
+
+        // ------------ Ejercicio 2 ------------
+        edad++;
+        System.out.println("Hoy es mi cumple y tengo " + edad + " años");
+
+    }
+}
